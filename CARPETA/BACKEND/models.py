b999b632dc96ca6b060b87150ba30aa530dc7140
AndresDAVM <andresf-fonsecaa@unilibre.edu.co>
2026-02-26 08:44:39 -0500

    Update models.py

--- CARPETA/BACKEND/models.py
+++ CARPETA/BACKEND/models.py
@@ -1,1 +1,14 @@
+from sqlalchemy import Column, Integer, String, Float
+from database import Base
 
+class Paciente(Base):
+    __tablename__ = "pacientes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String)
+    edad = Column(Integer)
+    frecuencia_cardiaca = Column(Integer)
+    saturacion = Column(Integer)
+    temperatura = Column(Float)
+    dolor = Column(Integer)
+    prioridad = Column(String)
